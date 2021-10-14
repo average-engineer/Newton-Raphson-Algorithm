0eafc90deef9451d2b68cf9576e71833f5ca657c
Ashutosh Mukherjee <ashutosh.mukherjeecpg@gmail.com>
2021-10-14 23:15:20 +0200

    Observed distribution of the converged roots in domain of interest

--- NewtonRaphson.m
+++ NewtonRaphson.m
@@ -4,76 +4,64 @@ close all
 clc
 format short
 
-%% Newton Raphson Method: My try
+%% Segregating initial guesses based on the root which is reached
+% Domain of interest: (0.7783 , 2.807)
+
+% Sample array of initial guesses
+init_arr = linspace(0.7783,2.807,50);
+
+% Newton Raphson Algorithm is applied for each of the initial guess in the
+% array
+
 % Symbolic Representation
 syms x % Independent Variable
 
-% Functionn Definition
+% Function Definition
 f(x) = x^4 - 3*x^3 + x^2 - 2*x + 2;
 % First Derivative of Function
 fprime = diff(f);
 
-% Initial Guess
-x0 = -50;
-
 % Maximun Number of Iterations
-num = 100;
+num = 20;
 
 % Solution Array
-soln = NaN(num,1);
+% soln = NaN(length(init_arr),num);
 
-% The first element of the solution array is the initial guess
-soln(1) = x0;
-
-% Function Array
-func(1) = subs(f,x0);
-
-% Required Tolerance
-tol = 0.000001;
-
-% Number of iterations used by algorithm
-count = 0;
-
-% Looping over the iterations
-for iter = 1:num-1
+for ii = 1:length(init_arr)
+   
+    % Initial Guess
+    x0 = init_arr(ii);
     
-    % Newton Raphson Algorithm
-    soln(iter + 1) = soln(iter) - (subs(f,soln(iter)))/(subs(fprime,soln(iter)));
-    % Computing the function for the iterated values
-    func(iter + 1) = subs(f,soln(iter + 1));
+    % The first element of the solution array is the initial guess
+    soln(ii,1) = x0;
+
+    % Function Array
+    func(ii,1) = subs(f,x0);
     
-    % If required tolerance is achieved, the algorithm is terminated
-    if abs(soln(iter + 1) - soln(iter)) <= tol
-        break; % terminate the for loop once the required tolerance is achieved
+    % Looping over the iterations
+    for iter = 1:num-1
+        
+        % Newton Raphson Algorithm
+        soln(ii,iter + 1) = soln(ii,iter) - (subs(f,soln(ii,iter)))/(subs(fprime,soln(ii,iter)));
+        % Computing the function for the iterated values
+        func(ii,iter + 1) = subs(f,soln(ii,iter + 1));
+    
+        % Final Converged Root
+        convRoot(ii) = soln(ii,end);
+        
     end
-    count = iter;
 end
 
-% Plotting Results
-% Iterated Independent Variables
+% Plotting results
 figure
-hold on
-plot(soln,'-*','linewidth',2)
+plot(init_arr,convRoot,'*')
 grid on
-xlabel('Number of Iterations')
-ylabel('Iterated Root')
+xlabel('Initial Guesses in Domain of Interest')
+ylabel('Converged Roots')
+    
 
-% Function Values for the iterated variables
-figure
-hold on
-plot(func,'-*','linewidth',2)
-grid on
-xlabel('Number of Iterations')
-ylabel('Function Value')
 
-% Actual Roots of the given polynomial
-disp(strcat('Actual Roots of the polynomial are:',num2str(roots([1,-3,1,-2,2]))))
 
-disp(strcat('The number of iterations used by the algorithm are:',num2str(count)));
 
-%% Segregating initial guesses based on the root which is reached
-
-% Sample array of initial guesses
-% init_arr = 10*rand(1,50);
 
 
